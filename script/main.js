0f94b96f8ce8b6c445eff95574aa7dbf327df143
Andriy Dyachyk <andriy.dyachyk98@gmail.com>
2023-05-31 15:58:47 +0200

    creato arrayTeam con le proprie properties

--- script/main.js
+++ script/main.js
@@ -1,1 +1,34 @@
 console.log('principi.js loaded');
+
+let teamArray = [
+    { 
+        'name': 'Wayne Barney',
+        'role': 'Founder & CEO',
+        'image': 'wayne-barnet-founder-ceo.jpg'   
+    },
+    { 
+        'name': 'Angela Carol',
+        'role': 'Chief Editor',
+        'image': 'angela-carol-chief-editor.jpg'   
+    },
+    { 
+        'name': 'Walter Gordon',
+        'role': 'Office Manager',
+        'image': 'walter-gordon-office-manager.jpg'   
+    },
+    { 
+        'name': 'Angela Lopez',
+        'role': 'Social Media Manager',
+        'image': 'angela-lopez-social-media-manager.jpg'   
+    },
+    { 
+        'name': 'Scott Estrada',
+        'role': 'Developer',
+        'image': 'scott-estrada-developer.jpg'   
+    },
+    { 
+        'name': 'Barbara Ramos',
+        'role': 'Graphic Designer',
+        'image': 'barbara-ramos-graphic-designer.jpg'   
+    }
+]
